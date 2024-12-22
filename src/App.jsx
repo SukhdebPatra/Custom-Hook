a5ec5dbd045eb383bddaf9aeb8222d094e6e27a2
Sukhdeb Patra <sukhdebpatra1@gmail.com>
2024-12-22 11:42:50 +0530

    Added currecncy converter but api api is not working have to find a new api for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import CurrencyConverter from './CurrencyConverter'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
-  return (
+  return ( 
     <>
-     
+    <CurrencyConverter/>
     </>
   )
 }
